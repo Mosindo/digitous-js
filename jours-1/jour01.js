9a4689ba14732a0633dcda918e50c8cf55389c92
Mosindo <64804043+Mosindo@users.noreply.github.com>
2020-12-07 16:42:30 +0100

    1 er commit exo js

---
+++ jours-1/jour01.js
@@ -0,0 +1,23 @@
+// 01- Hello Word
+console.log("Hello World !");
+// 02-String
+var test = "My name is Mosindo" ;
+console.log(test);
+// 03- Concatenation
+var name = "Mosindo";
+console.log("Nice to meet you " + name);
+// 04- String Lenght
+var testLength = "I'm very long !";
+console.log(testLength.length);
+// 05- Replace
+var food = "croissant is meh";
+console.log(food.replace("meh","so good"));
+// 06- Up and Down
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
+// 07- Split
+// 08- Template
