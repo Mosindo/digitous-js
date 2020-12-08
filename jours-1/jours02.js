d2118486ecfd4f3d010d9e791ef353aeafe84c0a
Mosindo <64804043+Mosindo@users.noreply.github.com>
2020-12-08 14:20:42 +0100

    exo jours 2

---
+++ jours-1/jours02.js
@@ -0,0 +1,21 @@
+// 01- Number
+var integer = 102;
+var float = 13.9;
+
+console.log(integer);
+console.log(float);
+// 02- Convert
+var basic = 34;
+console.log(basic);
+var stringified =basic.toString();
+console.log(stringified)
+// 03- Round
+// 04- Arithmétique
+// 05- Comparaison
+// 06- Condition
+// 07- Condition II
+// 08- Condition III
+// 09- Bonus 
+// 10- Bonus II
+// 01- Bonus III
+// 01- Bonus IV
