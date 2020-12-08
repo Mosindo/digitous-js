49424ce1b7e52079e429d00a4dd6c3c646182632
Mosindo <64804043+Mosindo@users.noreply.github.com>
2020-12-08 14:54:40 +0100

    exo 6 fini

--- jours-1/jours02.js
+++ jours-1/jours02.js
@@ -10,12 +10,64 @@ console.log(basic);
 var stringified =basic.toString();
 console.log(stringified)
 // 03- Round
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
+
 // 04- Arithmétique
+var test =12;
+var bis = 5;
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test % bis);
+console.log(test ** bis);
+
 // 05- Comparaison
+var test= 143 ;
+var bis = 219 ;
+console.log( test > bis);
+console.log( test < bis);
+console.log( test >= bis);
+console.log( test <= bis);
+console.log( test == bis);
+console.log( test === bis);
+console.log( test != bis);
+console.log( test !== bis);
+
 // 06- Condition
+var limit = 50 ;
+var score = 64 ;
+//Les blocs if / else servent à exécuter du code si quelque chose est vérifié / ou pas.
+if( score >= limit){
+    console.log("Ok good !");
+}
+else {
+    console.log("Oh nooo...");
+};
+
+score = 34;
+
+if( score >= limit){
+    console.log("Ok good !");
+}
+else {
+    console.log("Oh nooo ...");
+};
+/*s'écrit au minimum comme suit :
+if (condition) {
+	... // ce bloc s'exécute si la condition est vraie
+} else {
+	... // ce bloc s'exécute si la condition est fausse
+}
+*/
 // 07- Condition II
+var 
 // 08- Condition III
 // 09- Bonus 
 // 10- Bonus II
 // 01- Bonus III
 // 01- Bonus IV
+
+//A la fin rajouté des commentaire à mes exercices pour avoir des notes ecrite de mes résolution de problème.
