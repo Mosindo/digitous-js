8f03fd7920615b0d1719831bd82a1ca03fa53d6e
Mosindo <64804043+Mosindo@users.noreply.github.com>
2020-12-09 14:52:21 +0100

    exo 3 terminer

--- jours-1/jours03.js
+++ jours-1/jours03.js
@@ -1,12 +1,27 @@
-// 01-Array
+// 01- Array
 
 var fruits = ["mango","lemon","bluebrry"];
 //Un tableau (array) est une liste de valeurs. Chaque élément à l'intérieur du tableau possède un index (la position de l'élément dans le tableau)
 //Un tableau peut contenir toute sorte de valeurs : des strings, des nombres, des booleans, des fonctions, des objets, d'autres tableaux...
 console.log(fruits);
 
-// 02-Access
+// 02- Access
 //array[index] : permet d'afficher un element  de le tableau en appelant l'index correspondant
 var ingredients = ["eggs","milk","butter"];
 console.log(ingredients[1]);
 console.log(ingredients[2]);
+
+// 03- Add an remove
+var objects = ["pen","book","lamp"];
+
+objects.unshift("chair");
+console.log(objects);//La méthode unshift() ajoute un ou plusieurs éléments au début d'un tableau et renvoie la nouvelle longueur du tableau.
+
+objects.pop();
+console.log(objects)//La méthode pop() supprime le dernier élément d'un tableau et retourne cet élément. Cette méthode modifie la longueur du tableau.
+
+objects.push("laptop");
+console.log(objects);//La méthode push() ajoute un ou plusieurs éléments à la fin d'un tableau et retourne la nouvelle taille du tableau.
+objects.shift()
+console.log(objects)
+// 0
