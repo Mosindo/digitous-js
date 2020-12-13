780319af95fd3fe06562dc0651dd3b830c4ecec3
Mosindo <64804043+Mosindo@users.noreply.github.com>
2020-12-13 21:28:20 +0100

    exercice 2 terminer"

--- cours/hard-loop.js
+++ cours/hard-loop.js
@@ -6,9 +6,21 @@ var numbers = [];
 var count = 0;
 for(var i = 0; i <50;i++){
    var random = Math.floor(Math.random()* 100+50)
-   numbers.push(random)
+   numbers.push(random);
    count+= 1;
    
 }
 console.table(numbers)
 console.log(count)
+
+// 02 - Max
+var numbers = [];
+
+for(var i = 0; i <50;i++){
+   var random = Math.floor(Math.random()* 200+50)
+      numbers.push(random);
+   }
+
+   numbers.sort((a, b) => b-a);
+   console.table(numbers);
+   console.log(numbers[0]);
